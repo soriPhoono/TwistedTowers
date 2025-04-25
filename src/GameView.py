b8a46de71f910f5299fd22a7894d226dd501e9ae
soriphoono <soriphoono@protonmail.com>
2025-04-25 09:17:00 -0500

    Updated game code

---
+++ src/GameView.py
@@ -0,0 +1,156 @@
+import arcade
+
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+WINDOW_TITLE = "Twisted Towers"
+
+TILE_SCALE = 0.5
+
+PLAYER_MOVEMENT_SPEED = 5
+PLAYER_JUMP_SPEED = 10
+
+
+class GameView(arcade.Window):
+    def __init__(self):
+        """
+        Initialize a new GameView object.
+
+        Parameters:
+        None
+        """
+
+        super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+
+        self.background_color = arcade.csscolor.SKY_BLUE
+
+        self.collect_coin_sound = arcade.load_sound(
+            ":resources:sounds/coin1.wav")
+        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
+
+        self.player_sprite = None
+        self.player_list = None
+        self.platform_list = None
+        self.wall_list = None
+        self.coin_list = None
+        self.physics_engine = None
+        self.camera = None
+        self.gui_camera = None
+        self.score = 0
+        self.score_text = None
+
+    def setup(self):
+        """
+        Set up the game/level.
+
+        This method is called once when the game starts. It is the place
+        where you can initialize the variables and do all the setup that
+        you need to do.
+
+        Parameters:
+        None
+        """
+
+        self.player_sprite = arcade.Sprite(
+            ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png")
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 128
+
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.player_sprite)
+
+        self.platform_list = arcade.SpriteList(use_spatial_hash=True)
+        for x in range(0, 1250, 64):
+            platform = arcade.Sprite(
+                ":resources:images/tiles/grassMid.png", scale=TILE_SCALE)
+            platform.center_x = x
+            platform.center_y = 32
+            self.platform_list.append(platform)
+
+        self.wall_list = arcade.SpriteList(use_spatial_hash=True)
+        box_coordinates = [[512, 96], [256, 96], [768, 96]]
+        for coordinate in box_coordinates:
+            wall = arcade.Sprite(
+                ":resources:images/tiles/boxCrate_double.png", scale=TILE_SCALE
+            )
+            wall.position = coordinate
+            self.wall_list.append(wall)
+
+        self.coin_list = arcade.SpriteList(use_spatial_hash=True)
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, self.platform_list, walls=self.wall_list
+        )
+
+        self.camera = arcade.Camera2D()
+
+        self.gui_camera = arcade.Camera2D()
+        self.score = 0
+        self.score_text = arcade.Text(
+            text=f"Score: {self.score}",
+            x=0,
+            y=5,
+        )
+
+        pass
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed."""
+
+        match key:
+            case arcade.key.ESCAPE:
+                self.setup()
+            case arcade.key.UP | arcade.key.W:
+                if self.physics_engine.can_jump():
+                    self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                    arcade.play_sound(self.jump_sound)
+            case arcade.key.LEFT | arcade.key.A:
+                self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+            case arcade.key.RIGHT | arcade.key.D:
+                self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called whenever a key is released."""
+
+        match key:
+            case arcade.key.UP | arcade.key.W:
+                self.player_sprite.change_y = 0
+            case arcade.key.LEFT | arcade.key.A:
+                self.player_sprite.change_x = 0
+            case arcade.key.RIGHT | arcade.key.D:
+                self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time: float):
+        self.physics_engine.update()
+
+        coin_hit_list = arcade.check_for_collision_with_list(
+            self.player_sprite, self.coin_list
+        )
+
+        for coin in coin_hit_list:
+            coin.remove_from_sprite_lists()
+            arcade.play_sound(self.collect_coin_sound)
+            self.score += 75
+            self.score_text.text = f"Score: {self.score}"
+
+        self.camera.position = self.player_sprite.position
+
+    def on_draw(self):
+        """
+        Render the screen.
+
+        This method is called whenever the screen needs to be redrawn.
+        It clears the screen and draws the necessary elements.
+        """
+
+        self.clear()
+
+        self.camera.use()
+
+        self.player_list.draw()
+        self.platform_list.draw()
+        self.wall_list.draw()
+        self.coin_list.draw()
+
+        self.gui_camera.use()
+
+        self.score_text.draw()
---
+++ src/main.py
@@ -0,0 +1,16 @@
+import arcade
+
+import GameView
+
+def main():
+    """
+    Main entry point for the Twisted Towers game.
+
+    Creates a window and begins the arcade event loop.
+    """
+    window = GameView.GameView()
+    window.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
